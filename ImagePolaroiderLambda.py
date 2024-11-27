7c29c58f8fc7558c22b48111d4dbd107e4a804fb
Projit Chandra <projitchandra32@gmail.com>
2024-11-27 12:15:28 +0530

    Working local sample

--- ImagePolaroiderLambda.py
+++ ImagePolaroiderLambda.py
@@ -1,12 +1,13 @@
-from PIL import ImageOps, ImageDraw
+import uuid
+from uuid import UUID
+
+from PIL import ImageOps, ImageDraw, ImageFont, ImageFile
 from PIL import Image
 from PIL.ExifTags import TAGS
 import PIL
 PIL.Image.MAX_IMAGE_PIXELS = 933120000
 
-
-
-def add_margin(pil_img, top, right, bottom, left, color):
+def add_margin(pil_img:Image.Image, top:int, right:int, bottom:int, left:int, color:tuple) -> Image.Image:
     width, height = pil_img.size
     new_width = width + right + left
     new_height = height + top + bottom
@@ -14,35 +15,84 @@ def add_margin(pil_img, top, right, bottom, left, color):
     result.paste(pil_img, (left, top))
     return result
 
-if __name__ == "__main__":
-    with Image.open("") as im:
+def draw_text(pil_image: Image.Image, message: str, font_color: tuple, width_factor: float,height_factor: float, font: str, font_size: int) -> Image.Image:
+    W, H = pil_image.size
+    draw = ImageDraw.Draw(pil_image)
+    font = ImageFont.truetype(font, font_size)
+    _, _, w, h = draw.textbbox((0, 0), message, font=font)
+    draw.text(((W-w)*width_factor, (H-h)*height_factor), message, font=font, fill=font_color)
+    return pil_image
+
+
+def generate_polaroid(image_URL:str) -> Image.Image:
+    with Image.open(image_URL) as im:
 
         width = im.width
         height = im.height
-        context_height = height if height > width else width
+        context_size = height if height > width else width
 
-        polaroid_image = add_margin(ImageOps.exif_transpose(im), int(context_height*0.02), int(context_height*0.02), int(context_height*0.15), int(context_height*0.02), (255, 255, 255))
+        polaroid_image = add_margin(ImageOps.exif_transpose(im), int(context_size * 0.02), int(context_size * 0.02),
+                                    int(context_size * 0.15), int(context_size * 0.02), (255, 255, 255))
 
-        exifdata = im._getexif()
-        metadata_keys = ["ImageWidth", "ImageLength", "Make", "Model", "DateTime", "FocalLength", "MaxApertureValue", "ISOSpeedRatings", "ShutterSpeedValue"]
+        exif_data = im._getexif()
+        metadata_keys = ["Make", "Model", "DateTime", "ImageWidth", "ImageLength", "FocalLength", "MaxApertureValue",
+                         "ISOSpeedRatings", "ShutterSpeedValue"]
         metadata_dict = dict()
         # iterating over all EXIF data fields
-        for tag_id in exifdata:
+        for tag_id in exif_data:
             # get the tag name, instead of human unreadable tag id
             tag = TAGS.get(tag_id, tag_id)
-            data = exifdata.get(tag_id)
+            data = exif_data.get(tag_id)
             # decode bytes
             if isinstance(data, bytes):
                 data = data.decode()
             if tag in metadata_keys:
-                metadata_dict[tag] = data
+                metadata_dict[tag] = str(data)
 
         print(metadata_dict)
 
-        ImageDraw.Draw(
-            polaroid_image  # Image
-        ).text(
-            (0, 0),  # Coordinates
-            'Hello world!',  # Text
-            (0, 0, 0)  # Color
-        )
+        main_line = ""
+
+        if metadata_dict.get("Make") and metadata_dict.get("Model"):
+            main_line = (metadata_dict.get("Make") + " " + metadata_dict.get("Model")).title()
+
+        if metadata_dict.get("DateTime"):
+            if main_line:
+                main_line += "   |   " + metadata_dict.get("DateTime")
+            else:
+                main_line = metadata_dict.get("DateTime")
+
+        print("Main line :", main_line)
+
+        polaroid_image = draw_text(polaroid_image, main_line, (0, 0, 0), 0.5, 0.9,
+                                   "./fonts/lato.heavy.ttf", 90)
+
+        sub_line = metadata_dict.get("ImageWidth") + "x" + metadata_dict.get("ImageLength")
+
+        if metadata_dict.get("MaxApertureValue"):
+            sub_line += "   f/" + metadata_dict.get("MaxApertureValue")
+
+        if metadata_dict.get("ShutterSpeedValue"):
+            sub_line += "   1/" + str(1 / float(metadata_dict.get("ShutterSpeedValue")))
+
+        if metadata_dict.get("FocalLength"):
+            sub_line += "   " + metadata_dict.get("FocalLength") + "mm"
+
+        if metadata_dict.get("ISOSpeedRatings"):
+            sub_line += "   ISO" + metadata_dict.get("ISOSpeedRatings")
+
+        print("Sub line :", sub_line)
+        polaroid_image = draw_text(polaroid_image, sub_line, (128, 128, 128), 0.5, 0.95,
+                                   "./fonts/lato.light-italic.ttf", 70)
+
+        return polaroid_image
+
+if __name__ == "__main__":
+        generate_polaroid("E:\\photos\\20240112_164420.jpg").save("./output/"+str(uuid.uuid4())+".png")
+
+
+
+
+
+
+
