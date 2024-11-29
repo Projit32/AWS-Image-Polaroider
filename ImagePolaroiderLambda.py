109527364b094b89ab3e55f181ecf8cec273e679
Projit Chandra <projitchandra32@gmail.com>
2024-11-30 04:05:33 +0530

    insta mode added and minor changes to normal mode

--- ImagePolaroiderLambda.py
+++ ImagePolaroiderLambda.py
@@ -1,11 +1,12 @@
 import concurrent.futures
 import traceback
 
+from markdown_it.rules_inline import image
 from memory_profiler import profile
 import uuid
 import os
 
-from PIL import ImageOps, ImageDraw, ImageFont
+from PIL import ImageOps, ImageDraw, ImageFont, ImageFilter
 from PIL import Image
 from PIL.ExifTags import TAGS
 import PIL
@@ -27,78 +28,132 @@ def draw_text(pil_image: Image.Image, message: str, font_color: tuple, width_fac
     draw.text(((W-w)*width_factor, (H-h)*height_factor), message, font=font, fill=font_color)
     return pil_image
 
+def get_meta_data(im: Image) -> (str, str):
+    exif_data = im._getexif()
+    metadata_keys = ["Make", "Model", "DateTime", "ImageWidth", "ImageLength", "FocalLength", "MaxApertureValue",
+                     "ISOSpeedRatings", "ExposureTime"]
+    metadata_dict = dict()
+    # iterating over all EXIF data fields
+    if exif_data:
+        for tag_id in exif_data:
+            # get the tag name, instead of human unreadable tag id
+            tag = TAGS.get(tag_id, tag_id)
+            data = exif_data.get(tag_id)
+            if tag in metadata_keys:
+                # decode bytes
+                if isinstance(data, bytes):
+                    data = data.decode()
+                metadata_dict[tag] = str(data)
 
-def generate_polaroid(image_URL:str) -> Image.Image:
+        print(metadata_dict)
+
+        main_line = ""
+
+        if metadata_dict.get("Make") and metadata_dict.get("Model"):
+            main_line = (metadata_dict.get("Make") + " " + metadata_dict.get("Model")).title()
+
+        if metadata_dict.get("DateTime"):
+            if main_line:
+                main_line += "   |   " + metadata_dict.get("DateTime")
+            else:
+                main_line = metadata_dict.get("DateTime")
+
+        print("Main line :", main_line)
+
+
+
+        sub_line = ""
+        if metadata_dict.get("ImageWidth") and metadata_dict.get("ImageLength"):
+            sub_line = metadata_dict.get("ImageWidth") + "x" + metadata_dict.get("ImageLength")
+
+        if metadata_dict.get("MaxApertureValue"):
+            sub_line += "   f/" + metadata_dict.get("MaxApertureValue")
+
+        if metadata_dict.get("ExposureTime"):
+            sub_line += "   1/" + str(1 / float(metadata_dict.get("ExposureTime")))+"s"
+
+        if metadata_dict.get("FocalLength"):
+            sub_line += "   " + metadata_dict.get("FocalLength") + "mm"
+
+        if metadata_dict.get("ISOSpeedRatings"):
+            sub_line += "   ISO" + metadata_dict.get("ISOSpeedRatings")
+
+        print("Sub line :", sub_line)
+
+        return main_line, sub_line
+
+
+def get_factors(width:int, height:int, for_insta: bool) -> (float, float, float, float, float):
+
+    print("aspect ratio", (width / height).as_integer_ratio())
+
+    side_factor = 0.02
+    bottom_factor = 0.145
+    top_factor = 0.02
+
+    main_text_height_factor = 0.9
+    sub_text_height_factor = 0.94
+
+    if for_insta:
+        side_factor = 0
+        bottom_factor = 0.135
+        top_factor =  0.07
+
+        main_text_height_factor = 0.93
+        sub_text_height_factor = 0.96
+
+    if height > width:
+        print("Portrait image")
+
+        bottom_factor = 0.13
+        main_text_height_factor = 0.93
+        sub_text_height_factor = 0.955
+
+    return side_factor, top_factor, bottom_factor, main_text_height_factor, sub_text_height_factor
+
+def blur_burst_center_image(im:Image.Image) -> Image.Image:
+    blurred_image = im.filter(ImageFilter.GaussianBlur(200))
+    blurred_burst_image = blurred_image.resize((2*im.height, im.height))
+    blurred_burst_cropped_image = blurred_burst_image.crop((im.height*0.4, 0, im.height*1.6, im.height))
+    left_start = (blurred_burst_cropped_image.width-im.width)//2
+    blurred_burst_cropped_image.paste(im, (left_start, 0))
+    return blurred_burst_cropped_image
+
+
+def generate_polaroid(image_URL:str, for_insta: bool) -> Image.Image:
     with Image.open(image_URL) as im:
 
-        width = im.width
-        height = im.height
-        context_size = height if height > width else width
-        context_font_size = width if height > width else height
+        context_size = max(im.height, im.width)
+        image = ImageOps.exif_transpose(im)
+        print(image.size)
 
-        polaroid_image = add_margin(ImageOps.exif_transpose(im), int(context_size * 0.02), int(context_size * 0.02),
-                                    int(context_size * 0.15), int(context_size * 0.02), (255, 255, 255))
+        if for_insta and image.height > image.width:
+            print("Portrait Insta image")
+            image = blur_burst_center_image(image)
 
-        exif_data = im._getexif()
-        metadata_keys = ["Make", "Model", "DateTime", "ImageWidth", "ImageLength", "FocalLength", "MaxApertureValue",
-                         "ISOSpeedRatings", "ShutterSpeedValue"]
-        metadata_dict = dict()
-        # iterating over all EXIF data fields
-        if exif_data:
-            for tag_id in exif_data:
-                # get the tag name, instead of human unreadable tag id
-                tag = TAGS.get(tag_id, tag_id)
-                data = exif_data.get(tag_id)
-                if tag in metadata_keys:
-                    # decode bytes
-                    if isinstance(data, bytes):
-                        data = data.decode()
-                    metadata_dict[tag] = str(data)
+        side_factor, top_factor, bottom_factor, main_text_height_factor, sub_text_height_factor = get_factors(image.width, image.height, for_insta)
 
-            print(metadata_dict)
 
-            main_line = ""
+        polaroid_image = add_margin(image, int(context_size * top_factor), int(context_size * side_factor),
+                                    int(context_size * bottom_factor), int(context_size * side_factor), (255, 255, 255))
 
-            if metadata_dict.get("Make") and metadata_dict.get("Model"):
-                main_line = (metadata_dict.get("Make") + " " + metadata_dict.get("Model")).title()
+        context_font_size = min(polaroid_image.height, polaroid_image.width)
 
-            if metadata_dict.get("DateTime"):
-                if main_line:
-                    main_line += "   |   " + metadata_dict.get("DateTime")
-                else:
-                    main_line = metadata_dict.get("DateTime")
+        main_line, sub_line = get_meta_data(im)
 
-            print("Main line :", main_line)
+        polaroid_image = draw_text(polaroid_image, main_line.strip(), (0, 0, 0), 0.5, main_text_height_factor,
+                                   "./fonts/SamsungOne-700.ttf", int(context_font_size / 36.44))
 
-            polaroid_image = draw_text(polaroid_image, main_line.strip(), (0, 0, 0), 0.5, 0.9,
-                                       "./fonts/lato.heavy.ttf", int(context_font_size/44.44))
+        polaroid_image = draw_text(polaroid_image, sub_line.strip(), (128, 128, 128), 0.5, sub_text_height_factor,
+                                   "./fonts/SamsungOne-400.ttf", int(context_font_size / 50.14))
 
-            sub_line = ""
-            if metadata_dict.get("ImageWidth") and metadata_dict.get("ImageLength"):
-                sub_line = metadata_dict.get("ImageWidth") + "x" + metadata_dict.get("ImageLength")
-
-            if metadata_dict.get("MaxApertureValue"):
-                sub_line += "   f/" + metadata_dict.get("MaxApertureValue")
-
-            if metadata_dict.get("ShutterSpeedValue"):
-                sub_line += "   1/" + str(1 / float(metadata_dict.get("ShutterSpeedValue")))
-
-            if metadata_dict.get("FocalLength"):
-                sub_line += "   " + metadata_dict.get("FocalLength") + "mm"
-
-            if metadata_dict.get("ISOSpeedRatings"):
-                sub_line += "   ISO" + metadata_dict.get("ISOSpeedRatings")
-
-            print("Sub line :", sub_line)
-            polaroid_image = draw_text(polaroid_image, sub_line.strip(), (128, 128, 128), 0.5, 0.95,
-                                       "./fonts/lato.medium-italic.ttf", int(context_font_size/57.14))
 
         return polaroid_image
 
 error_items = list()
 def main(value):
     try:
-        generate_polaroid(value).save("./output/" + str(uuid.uuid4()) + ".png", "PNG", compress_level=1)
+        generate_polaroid(value, True).save("./output/" + str(uuid.uuid4()) + ".png", "PNG", compress_level=1)
     except Exception as e:
         traceback.print_exc()
         print("Exception", str(e))
@@ -111,8 +166,7 @@ if __name__ == "__main__":
 
     result = list()
     with concurrent.futures.ThreadPoolExecutor(max_workers=16) as exe:
-        # Maps the method 'cube' with a list of values.
-        result = exe.map(main, values)
+        exe.map(main, values)
 
     print("Final result size", len(list(result)))
     print("final error Size", len(error_items), error_items)
