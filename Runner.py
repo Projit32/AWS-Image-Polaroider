3a7778be376137afedf0360dd1cba3323d3e3d30
Projit Chandra <projitchandra32@gmail.com>
2025-05-06 19:59:26 +0530

    Updated runner logic

--- Runner.py
+++ Runner.py
@@ -6,17 +6,18 @@ import uuid
 from PolaroidBuilder import generate_polaroid
 from PolaroidSettings import ColorMode, PolaroidMode
 
-import resource
-resource.setrlimit(resource.RLIMIT_AS, (1000000000,1000000000))
+# import resource
+# resource.setrlimit(resource.RLIMIT_AS, (1000000000,1000000000))
 
 error_items = list()
 
 
 def main(data):
     try:
-        generate_polaroid(data["image"], data["type"], data["color"]).save("./output/" + str(uuid.uuid4()) + ".png",
-                                                                           "PNG",
-                                                                           compress_level=1)
+        print(data["image"].split("/")[-1].replace(".jpg", ""))
+        output_file_name = data["image"].split("/")[-1].replace(".jpg", "") + "_" + str(data["type"]) + "_" + str(data["color"]) + ".png"
+        output_image = generate_polaroid(data["image"], data["type"], data["color"])
+        output_image.save("./output/" + output_file_name,"PNG",compress_level=1)
     except Exception as e:
         traceback.print_exc()
         print("Exception", str(e))
@@ -31,10 +32,10 @@ if __name__ == "__main__":
     print("Initial Input Size", len(values))
 
     data = []
-
+    values = [values[0]]
     print("Preparing data")
     for image in values:
-        data.append({"image": image, "color": ColorMode.DARK, "type": PolaroidMode.INSTA_SQUARED_COMPACT})
+        data.append({"image": image, "color": ColorMode.LIGHT, "type": PolaroidMode.INSTA_SQUARED_COMPACT})
 
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as exe:
